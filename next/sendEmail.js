37791d2c55372908079edb531259b7329758ad58
scristo2 <77905570+scristo2@users.noreply.github.com>
2022-10-07 16:24:18 +0200

    Create sendEmail.js

---
+++ next/sendEmail.js
@@ -0,0 +1,31 @@
+/api/sendEmail.js
+https://www.npmjs.com/package/emailjs
+<<<<<npm i emailjs
+import { SMTPClient } from 'emailjs';
+
+export default async  function handler(req, res) {
+
+    const client = new SMTPClient({
+        user: 'admin@orfibesa.es',
+        password: '32)@#bI~1@54',
+        host: 'gnldm1074.siteground.biz',
+        ssl: true,
+        port : 465
+    });
+
+    try {
+        const message = await client.sendAsync({
+            text: '<h1>esto e sun test liquid</h1>',
+            from: '<admin@orfibesa.es>',
+            to: '<javier235hj@hotmail.com>',
+            cc: '<admin@orfibesa.es>',
+            subject: 'testing emailjs',
+        });
+        res.status(200).json({status : 'ok'});
+
+    } catch (err) {
+        
+        res.status(200).json({status : `failed : ${err}`});
+    }
+
+}
